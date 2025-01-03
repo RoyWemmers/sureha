37272d7ad1aee4e4c159e1881bf51d471f35422e
Roy Wemmers <roywemmers@MBP-van-Roy.home>
2025-01-03 17:26:20 +0100

    Fix switch entity implementation for indoor-only mode
    Changes:
    - Made switch inherit from CoordinatorEntity
    - Added proper entity availability handling
    - Improved device info and state handling
    - Added debug logging

--- custom_components/sureha/switch.py
+++ custom_components/sureha/switch.py
@@ -8,6 +8,7 @@ from homeassistant.components.switch import SwitchEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from surepy.entities import SurepyEntity
 from surepy.entities.pet import Pet as SurePet
 from surepy.enums import EntityType
@@ -27,45 +28,65 @@ async def async_setup_entry(
     spc: SurePetcareAPI = hass.data[DOMAIN][config_entry.entry_id][SPC]
 
     entities = []
+    _LOGGER.debug("Setting up Sure Petcare switches")
 
     for surepy_entity in spc.coordinator.data.values():
+        _LOGGER.debug(
+            "Found entity: %s (type: %s, id: %s)",
+            surepy_entity.name,
+            surepy_entity.type,
+            surepy_entity.id,
+        )
         if surepy_entity.type == EntityType.PET:
+            _LOGGER.debug("Adding indoor-only switch for pet: %s", surepy_entity.name)
             entities.append(IndoorOnlyModeSwitch(spc.coordinator, surepy_entity.id, spc))
 
+    _LOGGER.debug("Adding %d switches", len(entities))
     async_add_entities(entities, True)
 
 
-class IndoorOnlyModeSwitch(SwitchEntity):
+class IndoorOnlyModeSwitch(CoordinatorEntity, SwitchEntity):
     """Sure Petcare Indoor Only Mode Switch."""
+
+    _attr_has_entity_name = True
 
     def __init__(self, coordinator, _id: int, spc: SurePetcareAPI) -> None:
         """Initialize the switch."""
-        super().__init__()
-        self._coordinator = coordinator
+        super().__init__(coordinator)
         self._id = _id
         self._spc: SurePetcareAPI = spc
 
-        self._surepy_entity: SurePet = self._coordinator.data[self._id]
+        self._surepy_entity: SurePet = self.coordinator.data[self._id]
         self._attr_name = f"{self._surepy_entity.name} Indoor Only Mode"
         self._attr_unique_id = f"{self._surepy_entity.household_id}-{self._id}-indoor-only"
+        
+        # Set up device info
         self._attr_device_info = {
-            "identifiers": {(DOMAIN, self._id)},
+            "identifiers": {(DOMAIN, str(self._id))},
             "name": self._surepy_entity.name,
             "manufacturer": "Sure Petcare",
             "model": "Pet",
+            "via_device": (DOMAIN, f"household_{self._surepy_entity.household_id}"),
         }
+
+    @property
+    def available(self) -> bool:
+        """Return True if entity is available."""
+        return self._id in self.coordinator.data
 
     @property
     def is_on(self) -> bool:
         """Return true if indoor only mode is on."""
-        return self._surepy_entity.indoor_only_mode
+        return bool(self.coordinator.data[self._id].indoor_only_mode)
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn the indoor only mode on."""
+        _LOGGER.debug("Turning on indoor-only mode for pet: %s", self._surepy_entity.name)
         await self._spc.set_indoor_only_mode(self._id, True)
-        await self._coordinator.async_request_refresh()
+        await self.coordinator.async_request_refresh()
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn the indoor only mode off."""
+        _LOGGER.debug("Turning off indoor-only mode for pet: %s", self._surepy_entity.name)
         await self._spc.set_indoor_only_mode(self._id, False)
-        await self._coordinator.async_request_refresh()
+        await self.coordinator.async_request_refresh()
