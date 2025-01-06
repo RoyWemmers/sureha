4524ab6785475d021249663a2e782de910b9016a
Roy Wemmers <roywemmers@MBP-van-Roy.home>
2025-01-06 16:40:51 +0100

    Fix service registration
    Changes:
    - Fixed service registration in __init__.py
    - Added schema=None to service registrations
    - Fixed method names for indoor only mode

--- custom_components/sureha/__init__.py
+++ custom_components/sureha/__init__.py
@@ -193,15 +193,15 @@ class SurePetcareAPI:
             await self.coordinator.async_request_refresh()
 
         self.hass.services.async_register(
-            DOMAIN, SERVICE_SET_LOCK_STATE, handle_set_lock_state
+            DOMAIN, SERVICE_SET_LOCK_STATE, handle_set_lock_state, schema=None
         )
 
         self.hass.services.async_register(
-            DOMAIN, SERVICE_PET_LOCATION, handle_set_pet_location
+            DOMAIN, SERVICE_PET_LOCATION, handle_set_pet_location, schema=None
         )
 
         self.hass.services.async_register(
-            DOMAIN, SERVICE_SET_INDOOR_ONLY_MODE, handle_set_indoor_only_mode
+            DOMAIN, SERVICE_SET_INDOOR_ONLY_MODE, handle_set_indoor_only_mode, schema=None
         )
 
         self.hass.services.async_register(
